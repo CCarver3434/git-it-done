9d5e718905874a3f183e72cbf7df438fd1062e67
Christopher Carver <vick3434@comcast.net>
2021-07-25 12:17:09 -0500

    create a function to call the github search api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,7 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
   // prevent page from refreshing
   event.preventDefault();
 
@@ -21,17 +21,17 @@ var formSubmitHandler = function(event) {
   }
 };
 
-var getUserRepos = function(user) {
+var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
   fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
       // request was successful
       if (response.ok) {
         console.log(response);
-        response.json().then(function(data) {
+        response.json().then(function (data) {
           console.log(data);
           displayRepos(data, user);
         });
@@ -39,12 +39,12 @@ var getUserRepos = function(user) {
         alert("Error: " + response.statusText);
       }
     })
-    .catch(function(error) {
+    .catch(function (error) {
       alert("Unable to connect to GitHub");
     });
 };
 
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
@@ -77,9 +77,12 @@ var displayRepos = function(repos, searchTerm) {
     // check if current repo has issues or not
     if (repos[i].open_issues_count > 0) {
       statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        repos[i].open_issues_count +
+        " issue(s)";
     } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
     // append to container
@@ -88,6 +91,15 @@ var displayRepos = function(repos, searchTerm) {
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl);
 };
 
 // add event listeners to forms
